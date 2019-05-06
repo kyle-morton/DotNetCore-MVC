a04d9b7e7496e724b15b7a800dd59c8252076833
Kyle Morton <kylemortondevelopment@gmail.com>
2019-05-06 18:02:23 -0500

    added list views/ctrl for order & customer

--- mvc-app/mvc-app.logic/Models/Order.cs
+++ mvc-app/mvc-app.logic/Models/Order.cs
@@ -6,6 +6,7 @@ namespace mvc_app.logic.Models
     public class Order : ModelBase
     {
         public DateTime CreateDate { get; set; }
+        public string OrderNumber { get; set; }     
         public List<Item> Items { get; set; }
     }
 }
